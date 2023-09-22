45e3216ee1343ab1a647d2bba32f55f067f7ab48
Luis Miguel <oluismrs@gmail.com>
2023-09-22 03:09:54 -0300

    feat: add new graphql inputs

---
+++ src/main/java/com/luwis/application/graphql/inputs/LoginInput.java
@@ -0,0 +1,3 @@
+package com.luwis.application.graphql.inputs;
+
+public record LoginInput(String email, String password) {}
---
+++ src/main/java/com/luwis/application/graphql/inputs/SignupInput.java
@@ -0,0 +1,3 @@
+package com.luwis.application.graphql.inputs;
+
+public record SignupInput(String name, String email, String password) {}
