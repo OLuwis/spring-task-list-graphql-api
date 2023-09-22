2f0bcf1debaa4e0ea337c0c5769e2ff3cd1330e6
Luis Miguel <oluismrs@gmail.com>
2023-09-22 02:56:09 -0300

    feat: add custom user details service

---
+++ src/main/java/com/luwis/application/services/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.luwis.application.services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.luwis.application.models.CustomUserDetails;
+import com.luwis.application.repositories.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userRepository.findByEmail(username).orElse(null);
+        return new CustomUserDetails(user);
+    }
+    
+}
