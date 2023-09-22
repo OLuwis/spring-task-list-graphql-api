468e91cad305033fd0d292b581684ec3dda0dae7
Luis Miguel <oluismrs@gmail.com>
2023-09-22 02:50:46 -0300

    feat: new spring security jwt auth

---
+++ src/main/java/com/luwis/application/configs/SecurityConfig.java
@@ -0,0 +1,94 @@
+package com.luwis.application.configs;
+
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.luwis.application.services.CustomUserDetailsService;
+
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Value("${JWT_KEY}")
+    private String jwtKey;
+
+    private final CustomUserDetailsService customUserDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(
+                        csrf -> csrf.disable())
+                .authorizeHttpRequests(
+                        auth -> auth
+                            .requestMatchers(new AntPathRequestMatcher("/graphql")).permitAll()
+                            .requestMatchers(new AntPathRequestMatcher("/graphiql")).permitAll()
+                            .anyRequest().permitAll())
+                .sessionManagement(
+                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .userDetailsService(customUserDetailsService)
+                .oauth2ResourceServer(
+                        oauth2 -> oauth2
+                            .jwt(jwt -> jwt.decoder(jwtDecoder())))
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authManager(UserDetailsService userDetailsService) throws AuthenticationException {
+        var authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return new ProviderManager(authProvider);
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        var key = new SecretKeySpec(jwtKey.getBytes(), "HmacSHA512");
+        return NimbusJwtDecoder
+                .withSecretKey(key)
+                .macAlgorithm(MacAlgorithm.HS256)
+                .build();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        return new NimbusJwtEncoder(new ImmutableSecret<>(jwtKey.getBytes()));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
