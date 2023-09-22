007ac513e593f0cb48b8741e2c1921ffbc6484c4
Luis Miguel <oluismrs@gmail.com>
2023-09-22 02:55:19 -0300

    feat: update user repository location

--- src/main/java/com/luwis/application/repositories/UserRepository.java
+++ src/main/java/com/luwis/application/repositories/UserRepository.java
@@ -1,10 +1,11 @@
-package com.luwis.application.user;
-
-import org.springframework.stereotype.Repository;
+package com.luwis.application.repositories;
 
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.luwis.application.models.UserModel;
 
 @Repository
 public interface UserRepository extends CrudRepository<UserModel, Long> {
