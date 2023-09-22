36f6d6f29953db7653109eab795da5b891f9c867
Luis Miguel <oluismrs@gmail.com>
2023-09-22 02:52:50 -0300

    feat: add custom user details

---
+++ src/main/java/com/luwis/application/models/CustomUserDetails.java
@@ -0,0 +1,66 @@
+package com.luwis.application.models;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final UserModel user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        String authorities = "READ WRITE UPDATE DELETE";
+        return Arrays
+                .stream(authorities.split(" "))
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+    }
+
+    public Long getId() {
+        return user.getId();
+    }
+
+    public String getName() {
+        return user.getName();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public String getPassword() {
+        if (user == null) throw new UsernameNotFoundException("Not Found");
+        return user.getPassword();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
